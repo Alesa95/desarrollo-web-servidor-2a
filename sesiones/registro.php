ffc0a164fc441b8da99bd8f9eed2c60c16250595
Alesa95 <alejandra_santiago_95@hotmail.com>
2023-11-03 13:59:15 +0100

    primer commit

---
+++ sesiones/registro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require 'conexion.php' ?>
+</head>
+<body>
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $usuario = $_POST["usuario"];
+        $contrasena = $_POST["contrasena"];
+
+        $contrasena_cifrada = password_hash($contrasena, PASSWORD_DEFAULT);
+        
+        $sql = "INSERT INTO usuarios VALUES ('$usuario', '$contrasena_cifrada')";
+    
+        $conexion -> query($sql);
+    }
+    ?>
+    <div class="container">
+        <h1>Registrarse</h1>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">Usuario:</label>
+                <input class="form-control" type="text" name="usuario">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Contraseña:</label>
+                <input class="form-control" type="password" name="contrasena">
+            </div>
+            <input class="btn btn-primary" type="submit" value="Registrarse">
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
