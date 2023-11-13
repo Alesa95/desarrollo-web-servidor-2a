67b401f2428bb7141cfb7a834fd119b16882f3a8
Alesa95 <alejandra_santiago_95@hotmail.com>
2023-11-13 14:18:47 +0100

    actualizacion lunes 13

--- validacion_formularios/listado_peliculas2.php
+++ validacion_formularios/listado_peliculas2.php
@@ -27,33 +27,32 @@
     ?>
     <div class="container">
         <h1>Listado de películas</h1>
-    </div>
 
-    <div>
-        <table class="table table-striped table-hover">
-            <thead class="table table-dark">
-                <tr>
-                    <th>ID Película</th>
-                    <th>Título</th>
-                    <th>Fecha de estreno</th>
-                    <th>Edad recomendada</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                foreach($peliculas as $pelicula) {
-                    echo "<tr>";
-                    echo "<td>" . $pelicula -> id_pelicula . "</td>";
-                    echo "<td>" . $pelicula -> titulo . "</td>";
-                    echo "<td>" . $pelicula -> fecha_estreno . "</td>";
-                    echo "<td>" . $pelicula -> edad_recomendada . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
-    </div> 
-    
+        <div>
+            <table class="table table-striped table-hover">
+                <thead class="table table-dark">
+                    <tr>
+                        <th>ID Película</th>
+                        <th>Título</th>
+                        <th>Fecha de estreno</th>
+                        <th>Edad recomendada</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach($peliculas as $pelicula) {
+                        echo "<tr>";
+                        echo "<td>" . $pelicula -> id_pelicula . "</td>";
+                        echo "<td>" . $pelicula -> titulo . "</td>";
+                        echo "<td>" . $pelicula -> fecha_estreno . "</td>";
+                        echo "<td>" . $pelicula -> edad_recomendada . "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div> 
+    </div>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
